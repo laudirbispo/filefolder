fd49fea31516ff07c0021db9ba80ab9aac4c032d
Laudir Bispo <laudirbispo@outlook.com>
2018-04-03 14:20:35 -0300

    Uploading files

---
+++ src/FileAndFolder/Folder.php
@@ -0,0 +1,310 @@
+<?php declare(strict_types=1);
+namespace libs\laudirbispo\FileAndFolder;
+
+/**
+ * Copyright (c) Laudir Bispo  (laudirbispo@outlook.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     (c) Laudir Bispo  (laudirbispo@outlook.com)
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+use DirectoryIterator;
+use InvalidArgumentException;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+class Folder
+{
+	use TraitFileAndFolder;
+	
+	/**
+     * Path to Folder.
+     *
+     * @var string
+     */
+	protected $path = '';
+	
+	/**
+     * The chmmod for Linux platform
+     *
+     * @var int (octal)
+     */
+    const DEFAULT_MODE = 0755;
+	
+	public static $errors = array();
+	
+	public static $messages = array();
+	
+	public function __construct($path = null, $create = false, $mode = false)
+    {
+		//...
+    }
+	
+	/**
+	 * Check if the current path exists
+	 */
+	public static function exists (string $path) : bool
+	{
+		return (file_exists($path));
+	}
+	
+	/**
+	 * Returns true if the file exists and can be modified
+	 */
+	public static function isWritable (string $path) : bool
+	{
+		if (!self::exists($path))
+			return false;
+		
+		return (is_writable($path));
+	}
+	
+	/**
+	 * Create directorys
+	 */
+	public static function create (string $path, int $mode = self::DEFAULT_MODE) : bool
+	{
+		$path = self::normalize($path);
+		
+		if (self::exists($path))
+			return true;
+
+		$oldumask = umask(0);
+		$dir = mkdir($path, $mode); 
+		umask($oldumask); 
+		return $dir;
+	}
+	
+	/**
+     * Returns an array of nested directories and files in each directory
+     *
+     * @param string|null $path the directory path to build the tree from
+     * @param array|bool $exceptions Either an array of files/folder to exclude
+     *   or boolean true to not grab dot files/folders
+     * @param string|null $type either 'file' or 'dir'. Null returns both files and directories
+     * @return array Array of nested directories and files in each directory
+     */
+    public static function tree (string $path, $exceptions = false, $type = null)
+    {
+        if (!self::exists($path))
+			return false;
+		
+        $files = [];
+        $directories = [$path];
+
+        if (is_array($exceptions)) {
+            $exceptions = array_flip($exceptions);
+        }
+        $skipHidden = false;
+        if ($exceptions === true) {
+            $skipHidden = true;
+        } elseif (isset($exceptions['.'])) {
+            $skipHidden = true;
+            unset($exceptions['.']);
+        }
+
+        try {
+            $directory = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::KEY_AS_PATHNAME | RecursiveDirectoryIterator::CURRENT_AS_SELF);
+            $iterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::SELF_FIRST);
+        } catch (Exception $e) {
+            if ($type === null) {
+                return [[], []];
+            }
+
+            return [];
+        }
+
+        foreach ($iterator as $itemPath => $fsIterator) {
+            if ($skipHidden) {
+                $subPathName = $fsIterator->getSubPathname();
+                if ($subPathName{0} === '.' || strpos($subPathName, DIRECTORY_SEPARATOR . '.') !== false) {
+                    continue;
+                }
+            }
+            $item = $fsIterator->current();
+            if (!empty($exceptions) && isset($exceptions[$item->getFilename()])) {
+                continue;
+            }
+
+            if ($item->isFile()) {
+                $files[] = $itemPath;
+            } elseif ($item->isDir() && !$item->isDot()) {
+                $directories[] = $itemPath;
+            }
+        }
+        if ($type === null) {
+            return [$directories, $files];
+        }
+        if ($type === 'dir') {
+            return $directories;
+        }
+
+        return $files;
+    }
+	
+	/**
+     * Change the mode on a directory structure recursively. This includes changing the mode on files as well.
+     *
+     * @param string $path The path to chmod.
+     * @param int|bool $mode Octal value, e.g. 0755.
+     * @param bool $recursive Chmod recursively, set to false to only change the current directory.
+     * @param array $exceptions Array of files, directories to skip.
+     * @return bool Success.
+     */
+    public static function setChmod (string $path, 
+									 int $mode = self::DEFAULT_MODE, 
+									 bool $recursive = true, 
+									 array $exceptions = []
+									)
+    {
+        if (!self::exists($path))
+		{
+			self::$errors[] = "[{$path}] - Não existe.";
+			return false;
+		}
+		
+		if (!self::isValidChmod($mode))
+		{
+			self::$messages[] = sprintf('O valor %s, é inválido para chmod. O valor padrão %s, foi aplicado para o diretório %s.', $mode, self::DEFAULT_MODE, $path);
+		}
+
+        if ($recursive === false && is_dir($path)) 
+		{
+            //@codingStandardsIgnoreStart
+            if (@chmod($path, intval($mode, 8))) 
+			{
+                //@codingStandardsIgnoreEnd
+                self::$messages[] = sprintf('%s alterado para %s', $path, $mode);
+
+                return true;
+            }
+            self::$errors[] = sprintf('%s não alterado para %s', $path, $mode);
+            return false;
+        }
+
+        if (is_dir($path)) 
+		{
+            $paths = self::tree($path);
+
+            foreach ($paths as $type) 
+			{
+                foreach ($type as $fullpath) 
+				{
+                    $check = explode(DIRECTORY_SEPARATOR, $fullpath);
+                    $count = count($check);
+
+                    if (in_array($check[$count - 1], $exceptions)) 
+                        continue;
+
+                    //@codingStandardsIgnoreStart
+                    if (@chmod($fullpath, intval($mode, 8))) 
+                        //@codingStandardsIgnoreEnd
+                        self::$messages[] = sprintf('%s alterado para %s', $fullpath, $mode);
+                    else 
+                        self::$errors[] = sprintf('%s não alterado para %s', $fullpath, $mode);
+                }
+            }
+
+            if (empty(self::$errors)) 
+                return true;   
+        }
+
+        return false;
+    }
+	
+	/**
+     * Recursively Remove directories if the system allows.
+     *
+     * @param string|null $path Path of directory to delete
+     * @return bool Success
+     */
+    public static function delete (string $path = null)
+    {
+        if (!$path) 
+            return false;
+
+        if (is_dir($path)) {
+            try {
+                $directory = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::CURRENT_AS_SELF);
+                $iterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::CHILD_FIRST);
+            } catch (\Exception $e) {
+                return false;
+            }
+
+            foreach ($iterator as $item) {
+                $filePath = $item->getPathname();
+                if ($item->isFile() || $item->isLink()) {
+                    //@codingStandardsIgnoreStart
+                    if (@unlink($filePath)) {
+                        //@codingStandardsIgnoreEnd
+                        self::$messages[] = sprintf('%s removido.', $filePath);
+                    } else {
+                        self::$errors[] = sprintf('%s não removido.', $filePath);
+                    }
+                } elseif ($item->isDir() && !$item->isDot()) {
+                    //@codingStandardsIgnoreStart
+                    if (@rmdir($filePath)) {
+                        //@codingStandardsIgnoreEnd
+                        self::$messages[] = sprintf('%s removido.', $filePath);
+                    } else {
+                        self::$errors[] = sprintf('%s não removido', $filePath);
+                        return false;
+                    }
+                }
+            }
+
+            $path = rtrim($path, DIRECTORY_SEPARATOR);
+            //@codingStandardsIgnoreStart
+            if (@rmdir($path)) {
+                //@codingStandardsIgnoreEnd
+                self::$messages[] = sprintf('%s removido', $path);
+            } else {
+                self::$errors[] = sprintf('%s não removido', $path);
+                return false;
+            }
+        }
+
+        return true;
+    }
+	
+	
+	/**
+     * Returns $path with $element added, with correct slash in-between.
+     *
+     * @param string $path Path
+     * @param string|array $element Element to add at end of path
+     * @return string Combined path
+     */
+    public static function addPathElement($path, $element)
+    {
+        $element = (array)$element;
+        array_unshift($element, rtrim($path, DIRECTORY_SEPARATOR));
+        return implode(DIRECTORY_SEPARATOR, $element);
+    }
+	
+	/**
+     * Returns true if given $path is a Windows path.
+     *
+     * @param string $path Path to check
+     * @return bool true if windows path, false otherwise
+     */
+    public static function isWindowsPath(string $path = '') : bool
+    {
+        return (preg_match('/^[A-Z]:\\\\/i', $path) || substr($path, 0, 2) === '\\\\');
+    }
+ 
+	
+	public static function isValidChmod (int $mode) : bool
+	{
+		if (filter_var($mode, FILTER_VALIDATE_INT, array('flags' => FILTER_FLAG_ALLOW_OCTAL)))
+			return true;
+		else
+			return false;
+	}
+	
+}
---
+++ src/FileAndFolder/TraitFileAndFolder.php
@@ -0,0 +1,142 @@
+<?php declare(strict_types=1);
+namespace libs\laudirbispo\FileAndFolder;
+
+/**
+ * Copyright (c) Laudir Bispo  (laudirbispo@outlook.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     (c) Laudir Bispo  (laudirbispo@outlook.com)
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+trait TraitFileAndFolder
+{
+	/**
+	 * Normalize paths
+	 */
+	public static function normalize (string $path)
+	{
+		$path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
+        $parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
+        $absolutes = array();
+        foreach ($parts as $part) 
+		{
+            if ('.' == $part) continue;
+            if ('..' == $part) 
+                array_pop($absolutes);
+            else 
+                $absolutes[] = $part;     
+        }
+		$path = implode(DIRECTORY_SEPARATOR, $absolutes);
+		
+		if (self::isAbsolute($path))
+			return $path;
+		else 
+        	return $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . $path;	
+	}
+	
+	/**
+     * Returns true if given $path is an absolute path.
+     *
+     * @param string $path Path to check
+     * @return bool true if path is absolute.
+     */
+    public static function isAbsolute(string $path = '') : bool
+    {
+        if (empty($path)) 
+            return false;
+
+        return $path[0] === '/' ||
+            preg_match('/^[A-Z]:\\\\/i', $path) ||
+            substr($path, 0, 2) === '\\\\';
+    }
+	
+	/**
+	 * Generates a random name
+	 */
+	public static function generateRandomName () : string
+	{
+		return (string) date('Ymdhis').time().mt_rand(111, 999);
+	}
+	
+	/**
+	 * Slug name
+	 */
+	public static function slugify ($string) : string
+	{
+		if (!is_string($string))
+			return $string;
+		
+		$string = preg_replace('/[\t\n]/', ' ', $string);
+		$string = preg_replace('/\s{2,}/', ' ', $string);
+		$list = array(
+			'Š' => 'S', 'š' => 's', 'Đ' => 'Dj', 'đ' => 'dj', 'Ž' => 'Z', 'ž' => 'z', 'Č' => 'C', 'č' => 'c', 'Ć' => 'C', 'ć' => 'c', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'ý' => 'y', 'þ' => 'b', 'ÿ' => 'y', 'Ŕ' => 'R', 'ŕ' => 'r', '/' => '-', ' ' => '-', '.' => '-',
+		);
+
+		$string = strtr($string, $list);
+		$string = preg_replace('/-{2,}/', '-', $string);
+		$string = strtolower($string);
+		return $string;
+	}
+	
+	/**
+	 * Convert to human readable mode 
+	 *
+	 * @param string $bytes 
+	 * @return string
+	 */
+	public static function convertSizeToHumans ($bytes) 
+	{
+		if (!is_int($bytes))
+			return 'null';
+		
+		$bytes = floatval($bytes);
+        $arBytes = array(
+            0 => array("UNIT" => "TB", "VALUE" => pow(1024, 4)),
+            1 => array("UNIT" => "GB", "VALUE" => pow(1024, 3)),
+            2 => array("UNIT" => "MB", "VALUE" => pow(1024, 2)),
+            3 => array("UNIT" => "KB","VALUE" => 1024),
+            4 => array("UNIT" => "B", "VALUE" => 1),
+        );
+		
+		$result = '0 Kb';
+
+		foreach($arBytes as $arItem)
+		{
+			if($bytes >= $arItem["VALUE"])
+			{
+				$result = $bytes / $arItem["VALUE"];
+				$result = str_replace(".", "," , strval(round($result, 2)))." ".$arItem["UNIT"];
+				break;
+			}
+		}
+		return $result;
+		
+	}
+	
+	/**
+	 * Verify that the file name is valid
+	 *
+	 * @return - true if the name is valid
+	 */
+	public static function isValidName (string $filename) : bool 
+	{
+    	return (bool) (preg_match("`^[-0-9A-Z_\.]+$`i", $filename)) ? true : false ;
+	}
+		
+	/**
+	 * Make sure the file name lenght is valid
+	 * The maximum number of characteres allowed is 255
+	 *
+	 * @return - true if the name is valid
+	 */	
+	public static function isValidNameLenght (strinf $filename) : bool
+	{
+		return (bool) (mb_strlen($filename,"UTF-8") > 225) ? true : false ;
+	}
+	
+}
